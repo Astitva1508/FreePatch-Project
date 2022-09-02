59f8c041ad4f8873506e6b28088aed027ecca9e7
Astitva1508 <100833642+Astitva1508@users.noreply.github.com>
2022-09-02 07:57:38 +0530

    Fixed further security bugs

---
+++ client/build/precache-manifest.96b83069705bdb0229bea323f8961c7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "561a42337bf234e4f6ce",
+    "url": "/static/css/main.99b9b3d6.chunk.css"
+  },
+  {
+    "revision": "561a42337bf234e4f6ce",
+    "url": "/static/js/main.561a4233.chunk.js"
+  },
+  {
+    "revision": "665bbf9a6e88a022f7ab",
+    "url": "/static/js/1.665bbf9a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ff629c02385f2f3a199b7e0b3065d077",
+    "url": "/static/media/googleLogo.ff629c02.png"
+  },
+  {
+    "revision": "7571ea13179d06d922f912f64d14abc6",
+    "url": "/static/media/twitterLogo.7571ea13.png"
+  },
+  {
+    "revision": "b0ae779e09adb9e411af212f03d36ad2",
+    "url": "/index.html"
+  }
+];
