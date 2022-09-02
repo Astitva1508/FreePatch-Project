85608fad799d8afa49420c770f1ac30d90e7626f
Astitva1508 <100833642+Astitva1508@users.noreply.github.com>
2022-09-02 14:55:13 +0530

    Changed the index.js file

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const errorHandlerMiddleware=require('./middleware/errorHandler')
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
-app.use('/',(req,res)=>{
+app.get('/',(req,res)=>{
     res.send('I am an entreprenuer')
 })
 
